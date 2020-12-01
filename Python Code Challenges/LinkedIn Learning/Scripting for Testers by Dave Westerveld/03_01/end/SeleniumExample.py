a2026c289da378f3a9d4d72d67de948942f263bd
Dmytro Verbytskyi <dmytro.qa.engineer@gmail.com>
2020-12-01 17:41:19 +0200

    selenium continue

--- Python Code Challenges/LinkedIn Learning/Scripting for Testers by Dave Westerveld/03_01/end/SeleniumExample.py
+++ Python Code Challenges/LinkedIn Learning/Scripting for Testers by Dave Westerveld/03_01/end/SeleniumExample.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
-driver.get('http://google.com')
+driver.get('https://google.com')
 
 element = driver.find_element_by_name('q')
-
 element.send_keys("test")
-
 element.send_keys(Keys.RETURN)
