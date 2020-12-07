717ee453d80332091e2abb0303e5ec03b39f0f2f
Dmytro Verbytskyi <dmytro.qa.engineer@gmail.com>
2020-12-07 13:36:47 +0200

    selenium page set up

--- Python Code Challenges/LinkedIn Learning/Scripting for Testers by Dave Westerveld/03_03/begin/SeleniumDataChecking.py
+++ Python Code Challenges/LinkedIn Learning/Scripting for Testers by Dave Westerveld/03_03/begin/SeleniumDataChecking.py
@@ -2,4 +2,15 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-driver.get('http://www.phptravels.net/offers')
+driver.get('https://www.hackerrank.com/dashboard')
+
+skill_tags = driver.find_elements_by_class_name('track-name')
+
+available_skills = []
+for skill in skill_tags:
+    available_skills.append(skill.text)
+
+for skill in available_skills:
+    print(skill)
+
+driver.close()
--- Python Code Challenges/LinkedIn Learning/Scripting for Testers by Dave Westerveld/03_04/begin/SeleniumPageSetup.py
+++ Python Code Challenges/LinkedIn Learning/Scripting for Testers by Dave Westerveld/03_04/begin/SeleniumPageSetup.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
 
 url = 'http://automationpractice.com/index.php?id_category=3&controller=category'
 driver.get(url)
+
+product_containers = driver.find_elements_by_class_name('product-container')
+
+for index, product in enumerate(product_containers):
+    hover = ActionChains(driver).move_to_element(product)
+    hover.perform()
+    driver.find_element_by_xpath('//*[@id="center_column"]/ul/li[%s]/div/div[2]/div[2]/a[1]/span' % (index+1)).click()
+    time.sleep(1)
+    element = WebDriverWait(driver, 2)
+
+    driver.find_element_by_xpath('//*[@id="layer_cart"]/div[1]/div[2]/div[4]/span').click()
+    time.sleep(1)
