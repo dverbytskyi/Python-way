2c6cdf3e80cf9b6d0e1822d9c490ed9cf15fb72d
Dmytro Verbytskyi <dmytro.qa.engineer@gmail.com>
2021-07-15 16:18:43 +0300

    fixed Authentication.py

--- Python Code Challenges/API calls/Authentication.py
+++ Python Code Challenges/API calls/Authentication.py
@@ -4,7 +4,7 @@ url = 'https://api.github.com/user'
 
 
 def get_user():
-    response = requests.get(url, headers={'Authorization': 'Bearer {token}}'})
+    response = requests.get(url, headers={'Authorization': 'Bearer {token}'})
     # print(response.status_code, response.json())
     my_json = response.json()
     for key, value in my_json.items():
